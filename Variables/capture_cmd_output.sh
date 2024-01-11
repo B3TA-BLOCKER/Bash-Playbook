4f095555d1811571a55a6bdecbf69b82fd57126d
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-11 22:38:01 +0500

    Add files via upload

---
+++ Variables/capture_cmd_output.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+# Create a variable
+# Type in the dollar sign ($) 
+# and in parentheses, type in the command whose output you want to store
+
+files=$(pwd)
+
+# if we echo the contents of the variable 
+# we'll see the output of the command in b/w the parentheses
+
+echo $files
