4ac68052b6adea8cc203fdba8e7b74fcbd5f45b4
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-11 22:36:28 +0500

    Add files via upload

---
+++ Variables/variable_usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variable allows us to store information for later use.
+
+# Dclaring a variable 
+# (You type whatever you want to name your variable and then include the equal sign{=} with no space and assign the variable with some value or text).
+
+myname="Hassaan"
+myage="20"
+
+# one way we can reference the variable is to print its contents and we can do it with the {echo} command 
+
+# echo myname
+# This isn't going to work when ever we reference a variable in bash we have to include a dollar sign in front of the name of the variable 
+
+echo "My name is $myname."
+echo "I'm $myage years old"
