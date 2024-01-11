bfe8db4de4f2effa2eb1bcd69a29e6c1cc659bf0
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-11 22:37:43 +0500

    Add files via upload

---
+++ Variables/system_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello"
+now=$(date)
+echo "The system time and date is : $now"
+
+# Within your environment, there are many default variables that are already declared.
+# $USER is one of those.
+
+echo "Your Username is $USER"
