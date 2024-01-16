d117dec94ca6013efb27eaa4c9bfdaaf640a43e2
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-16 18:58:17 +0500

    Add files via upload

---
+++ If Statements/if_else_Statement.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+val=300
+
+if [ $val -eq 300 ]
+then
+    echo "The condition is true "
+else
+    echo "The variable does not equal 300 "
+fi # to end the if statement we write the (fi) keyword
