58836be39a5bdd5f78edd5b387a375cbe60c5ba7
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-16 18:46:12 +0500

    Add files via upload

---
+++ If Statements/Simple_if_statement.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# declaring a variable and giving it a value
+val=200
+
+# the space between the dollar sign and the bracket is important, and we need that space 
+
+if [ $val -eq 200 ]
+then 
+    echo "The condition is true"
+fi
