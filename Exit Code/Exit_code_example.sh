a50e0782f84768571b750e2ff2e9e26bb8e459e4
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-17 16:13:26 +0500

    Add files via upload

---
+++ Exit Code/Exit_code_example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The exit code allows us to determine whether the command we entered was successful or not.
+
+package=htop
+
+sudo apt install $package
+
+echo "The exit code of the package install is : $?"
+
