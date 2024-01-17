5013369f24a551c199bc2d8bacecfd1f54037953
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-17 17:08:17 +0500

    Add files via upload

---
+++ Exit Code/example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+directory=/etc
+
+if [ -d $directory ]
+then
+    echo "The directory $directory exists."
+    exit 0
+else
+    echo "The directory $directory doesn't exists."
+fi
+
+echo "The exit code for this script run is : $?"
+echo "You didn't see that statement."
+echo "You won't see this one either."
