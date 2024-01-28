2a032bf218dcda9e872f89114b149735accfde94
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-01-28 16:36:23 +0500

    Add files via upload

---
+++ For Loops/For_Loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# What is a FOR Loop ?
+
+# It allows you to perform a task repeatedly for every item in a set
+clear 
+
+for current_number in 1 2 3 4 5 6 7 8 9 10
+do 
+    echo $current_number
+    sleep 1
+done
+
+echo "This is outside of the for loop."
