76745ade3fb19c0167b413af27c5302ece7e5bac
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-03 11:08:21 +0500

    Update Utilizing_Standard_Output.sh

--- Data Streams/Utilizing_Standard_Output.sh
+++ Data Streams/Utilizing_Standard_Output.sh
@@ -8,7 +8,7 @@ errorlog=/var/log/update_errors.log
 
 if grep -q "Arch" $release_file 
 then
-    sudo pacman -Syu -y  1>>$longfiles 2>>$errorlog
+    sudo pacman -Syu -y  1>>$longfiles 2>>$errorlog  # This comand will run in the background and no output will be shown on the screen
     if [ $? -ne 0 ]  # (ne) means "Not equals to Zero"
     then 
         echo "An error occured, please check the $errorlog files."
@@ -17,8 +17,8 @@ fi
 
 if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file
 then 
-    sudo apt update -y 1>>$longfiles 2>>$errorlog
-    sudo apt dist-upgrade -y 1>>$longfiles 2>>$errorlog
+    sudo apt update -y 1>>$longfiles 2>>$errorlog   # This comand will run in the background and no output will be shown on the screen
+    sudo apt dist-upgrade -y 1>>$longfiles 2>>$errorlog   # This comand will run in the background and no output will be shown on the screen
     if [ $? -ne 0 ]  # (ne) means "Not equals to Zero"
     then 
         echo "An error occured, please check the $errorlog files."
