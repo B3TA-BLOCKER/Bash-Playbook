4a7b3022497e9eefeb00971fe9da935df99e78cf
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-03 12:00:24 +0500

    Add files via upload

---
+++ Case Statements/Example_of_Case_Statements.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "What is your favorite Linux Distribution? "
+
+echo "1 - Arch"
+echo "2 - CentOS"
+echo "3 - Debian"
+echo "4 - Mint"
+echo "5 - Ubuntu"
+echo "6 - Something else..."
+
+read distro;
+
+case $distro in 
+    1) echo "Arch is a rolling release.";; # These case statement will not work eithout these semi colons 
+    2) echo "CentOS is popular on servers.";;
+    3) echo "Debian is a community distribution.";;
+    4) echo "Mint is popular on desktops and laptops.";;
+    5) echo "Ubuntu is popular on both servers and computers.";;
+    6) echo "There are many distribution out there.";;
+    *) echo "You didn't enter an appropriate choice." # The last line dont need a semi colon
+esac
