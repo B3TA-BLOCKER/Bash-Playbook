693e8373ae452f068562b8634a66c869899079dd
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-03 10:37:56 +0500

    Update Utilizing_Standard_Output.sh

--- Data Streams/Utilizing_Standard_Output.sh
+++ Data Streams/Utilizing_Standard_Output.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+# Optimizing "Universal_update.sh" script
+
 release_file=/etc/os-release
 longfiles=/var/log/update.log
 errorlog=/var/log/update_errors.log
 
-# Depending on which OS you are running you can change the search criteria
-
-if grep -q "Arch" $release_file # I want to search for the term "Arch" inside the release_file  
+if grep -q "Arch" $release_file 
 then
-    # The host is based on Arch, run the pacman update command
     sudo pacman -Syu -y  1>>$longfiles 2>>$errorlog
     if [ $? -ne 0 ]  # (ne) means "Not equals to Zero"
     then 
@@ -18,8 +17,6 @@ fi
 
 if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file
 then 
-    # The host is based on Debian or Ubuntu ,
-    # Run the apt version of the command
     sudo apt update -y 1>>$longfiles 2>>$errorlog
     sudo apt dist-upgrade -y 1>>$longfiles 2>>$errorlog
     if [ $? -ne 0 ]  # (ne) means "Not equals to Zero"
