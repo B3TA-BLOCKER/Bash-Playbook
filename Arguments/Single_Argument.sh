1af58f9fff9718906b8de6d7d0921175e5cdf1bc
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-04 22:15:36 +0500

    Update Single_Argument.sh

--- Arguments/Single_Argument.sh
+++ Arguments/Single_Argument.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 echo "You Entered the argument: $1" # $1 is an argument 
 
 # The difference b/w an "argument" and a "variable" is that an argument is declared when you run the script.
