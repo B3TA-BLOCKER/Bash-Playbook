8f3f7615813801f1110e7a58b04b028fa4cecf83
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-04 19:05:08 +0500

    Add files via upload

---
+++ Scheduling Jobs/Part 1/Timed_Task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# use the at command to run this script at your desirable time
+# usw this command (at "time" -f ./script_name)
+# using the (atq) command will tell us time at which this script has executed
+# using the (atrm) command followed by the job number 
+
+logfile=job_results.log
+
+echo "The script run at the following time: $(date)" > $logfile
