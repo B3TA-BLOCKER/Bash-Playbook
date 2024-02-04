7f1f6d267c0367f77d8c37386de77effb655d6d1
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-04 22:15:04 +0500

    Add files via upload

---
+++ Arguments/Single_Argument.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "You Entered the argument: $1" # $1 is an argument 
+
+# The difference b/w an "argument" and a "variable" is that an argument is declared when you run the script.
