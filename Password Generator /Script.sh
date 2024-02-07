a0466fd5ea7b87b3588df89f889213d9724dbd23
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-08 01:49:49 +0500

    Update Script.sh

--- Password Generator /Script.sh
+++ Password Generator /Script.sh
@@ -12,3 +12,12 @@ echo -e "\t\t\t\t\t\t\t   ######  #    #  ####   ####  #    # #    # #    # #   
 echo -e "\t\t\t\t\t\t\t   #       ######      #      # # ## # #    # #####  #    #    #     # #      #  # # #      #####  ######   #   #    # #####  "
 echo -e "\t\t\t\t\t\t\t   #       #    # #    # #    # ##  ## #    # #   #  #    #    #     # #      #   ## #      #   #  #    #   #   #    # #   #  "
 echo -e "\t\t\t\t\t\t\t   #       #    #  ####   ####  #    #  ####  #    # #####      #####  ###### #    # ###### #    # #    #   #    ####  #    # "
+
+# User is going to enter the length of the password.
+echo -n -e "Please Enter the length of the password \n-> "
+read Password_Lenght
+
+for P in $(seq 1);
+do
+    openssl rand -base64 48 | cut -c1-$Password_Length
+done
